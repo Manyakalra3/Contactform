e11a7f3ee3dd2b707695ce0c49a0cc8a98bfa701
Manya kalra <73202244+Manyakalra3@users.noreply.github.com>
2021-11-27 01:45:43 +0530

    final

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
-app.use(cors({ origin: "*" }));
+
 
 const transporter = nodemailer.createTransport({
     // host: "smtp.gmail.com", 
